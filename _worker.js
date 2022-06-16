7c05b714c76f5ead2a72290e169c683fd7958176
wespochac <61771278+wespochac@users.noreply.github.com>
2022-06-16 22:23:43 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -2,7 +2,7 @@ export default {
     async fetch(request, env) {
       let url = new URL(request.url);
       if (url.pathname.startsWith('/')) {
-        url.hostname="www.cloudflare.com";
+        url.hostname="104.16.124.96";
         let new_request=new Request(url,request);
         return fetch(new_request);
       }
