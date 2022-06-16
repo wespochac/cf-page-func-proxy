0e105e67abcbdd94eb07171ac3ab5937b52d764b
wespochac <61771278+wespochac@users.noreply.github.com>
2022-06-16 22:44:08 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -2,7 +2,7 @@ export default {
     async fetch(request, env) {
       let url = new URL(request.url);
       if (url.pathname.startsWith('/')) {
-        url.hostname="hk.riverfront.ga";
+        url.hostname="colo.fallplaces.com";
         let new_request=new Request(url,request);
         return fetch(new_request);
       }
